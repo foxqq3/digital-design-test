ba128bfbce3bd2b35789d1cfe2c984fc1593704d
foxqq3 <dmitriifoxqq2@gmail.com>
2023-06-04 14:52:59 +0300

    Change  data function: delete not warking part

--- js/getDayInfo.js
+++ js/getDayInfo.js
@@ -23,21 +23,17 @@ export default function getDayInfo(value) {
     'Декабря',
   ];
 
-    const currentDate = new Date(value);
-    const dayOfMonth = currentDate.getDate();
-    const dayOfWeek = currentDate.getDay();
-    const monthOfYear = currentDate.getMonth();
-    const year = currentDate.getFullYear();
-    let weekOfMonth = getWeekOfMonth();
+  const currentDate = new Date(value);
+  const dayOfMonth = currentDate.getDate();
+  const dayOfWeek = currentDate.getDay();
+  const monthOfYear = currentDate.getMonth();
+  const year = currentDate.getFullYear();
+  let weekOfMonth = getWeekOfMonth();
 
-    function getWeekOfMonth() {
-      const fixedDayOfWeek = dayOfWeek === 0 ? 7 : dayOfWeek;
-      if (dayOfMonth === fixedDayOfWeek) {
-        return Math.ceil((dayOfMonth - fixedDayOfWeek - 1) / 7) + 1;
-      } else {
-        return Math.ceil((dayOfMonth - fixedDayOfWeek) / 7) + 1;
-      }
-    }
+  function getWeekOfMonth() {
+    const fixedDayOfWeek = dayOfWeek === 0 ? 7 : dayOfWeek;
+    return Math.ceil((dayOfMonth - fixedDayOfWeek) / 7) + 1;
+  }
 
   return `${days[dayOfWeek]}, ${weekOfMonth} неделя ${months[monthOfYear]} ${year} года`;
 }
